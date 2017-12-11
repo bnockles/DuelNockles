37e159ad24e7988a36c16b5312f141280fa35204
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-12-11 15:18:28 -0500

    first edit

---
+++ DuelNockles/src/sortomania/contestants/DerekDavid.java
@@ -0,0 +1,94 @@
+package sortomania.contestants;
+
+import java.awt.Color;
+
+import sortomania.Contestant;
+
+public class DerekDavid extends Contestant {
+
+	static int[] asdf = { 1, 2, 3, 4, 5, 6 };
+
+	public static void main(String[] args) {
+		DerekDavid asd = new DerekDavid();
+		System.out.print();
+	}
+
+	@Override
+	public Color getColor() {
+		return new Color(104, 152, 225);
+	}
+
+	@Override
+	public String getSpriteName() {
+		return RYU;
+	}
+
+	public int sortAndGetMedian(int[] random) {
+		int[] numArray = sort(random);
+		double median;
+		if (numArray.length % 2 == 0)
+			median = ((double) numArray[numArray.length / 2] + (double) numArray[numArray.length / 2 - 1]) / 2;
+		else
+			median = (double) numArray[numArray.length / 2];
+		return median;
+	}
+
+	@Override
+	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
+
+		return 0;
+	}
+
+	@Override
+	public double mostlySortAndGetMedian(int[] mostlySorted) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double sortMultiDim(int[][] grid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sortAndSearch(Comparable[] arr, Comparable toFind) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static int[] sort(int arr[]) {
+		int n = arr.length;
+
+		// The output character array that will have sorted arr
+		int output[] = new int[n];
+
+		// Create a count array to store count of inidividul
+		// characters and initialize count array as 0
+		int count[] = new int[256];
+		for (int i = 0; i < 256; ++i)
+			count[i] = 0;
+
+		// store count of each character
+		for (int i = 0; i < n; ++i)
+			++count[arr[i]];
+
+		// Change count[i] so that count[i] now contains actual
+		// position of this character in output array
+		for (int i = 1; i <= 255; ++i)
+			count[i] += count[i - 1];
+
+		// Build the output character array
+		for (int i = 0; i < n; ++i) {
+			output[count[arr[i]] - 1] = (int) arr[i];
+			--count[arr[i]];
+		}
+
+		// Copy the output array to arr, so that arr now
+		// contains sorted characters
+		for (int i = 0; i < n; ++i)
+			arr[i] = output[i];
+		return output;
+	}
+
+}
