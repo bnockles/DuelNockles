0d7a5b293ac15ce7ce4f615f7a8edde6b028921e
RPong1 <rpong8254@bths.edu>
2017-12-11 17:30:25 -0500

    need get median method

--- DuelNockles/src/sortomania/GarrettRicky.java
+++ DuelNockles/src/sortomania/GarrettRicky.java
@@ -23,44 +23,53 @@ public class GarrettRicky extends Contestant {
 
 	@Override
 	public double sortAndGetMedian(int[] random) {
-			Comparable[] tmp = new Comparable[random.length];
-			mergeSort(random, tmp,  0,  random.length - 1);
+	    if (random.length > 1) {
+            // split array into two halves
+            int[] left = leftHalf(random);
+            int[] right = rightHalf(random);
+            
+            // recursively sort the two halves
+            sortAndGetMedian(left);
+            sortAndGetMedian(right);
+            
+            // merge the sorted halves into a sorted whole
+            merge(random, left, right);
+	    	}
+	    //INSERT GETMEDIAN METHOD//
 		}
+	    public static int[] leftHalf(int[] array) {
+	        int size1 = array.length / 2;
+	        int[] left = new int[size1];
+	        for (int i = 0; i < size1; i++) {
+	            left[i] = array[i];
+	        }
+	        return left;
+	    }
+	    public static int[] rightHalf(int[] array) {
+	        int size1 = array.length / 2;
+	        int size2 = array.length - size1;
+	        int[] right = new int[size2];
+	        for (int i = 0; i < size2; i++) {
+	            right[i] = array[i + size1];
+	        }
+	        return right;
+	    }
+	    
+	    public static void merge(int[] result, int[] left, int[] right) {
+	    	int i1 = 0;   // index into left array
+	    	int i2 = 0;   // index into right array
 
-
-		private static void mergeSort(int[] random, Comparable [ ] tmp, int left, int right)
-		{
-			if( left < right )
-			{
-				int center = (left + right) / 2;
-				mergeSort(random, tmp, left, center);
-				mergeSort(random, tmp, center + 1, right);
-				merge(random, tmp, left, center + 1, right);
-			}
-	}
-
-		 private static void merge(int[] random, Comparable[ ] tmp, int left, int right, int rightEnd )
-		    {
-		        int leftEnd = right - 1;
-		        int k = left;
-		        int num = rightEnd - left + 1;
-
-		        while(left <= leftEnd && right <= rightEnd)
-		            if(random[left].compareTo(random[right]) <= 0)
-		                tmp[k++] = random[left++];
-		            else
-		                tmp[k++] = random[right++];
-
-		        while(left <= leftEnd)    // Copy rest of first half
-		            tmp[k++] = random[left++];
-
-		        while(right <= rightEnd)  // Copy rest of right half
-		            tmp[k++] = random[right++];
-
-		        // Copy tmp back
-		        for(int i = 0; i < num; i++, rightEnd--)
-		            random[rightEnd] = tmp[rightEnd];
-		    }
+	    	for (int i = 0; i < result.length; i++) {
+	    		if (i2 >= right.length || (i1 < left.length && left[i1] <= right[i2])) {
+	    			result[i] = left[i1];    // take from left
+	    			i1++;
+	    		} else {
+	    			result[i] = right[i2];   // take from right
+	    			i2++;
+	    		}
+	    	}
+	    }
+	    
 	@Override
 	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
 		// TODO Auto-generated method stub
