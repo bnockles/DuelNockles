e50611b782ebf6505a68f84154118ecc5115e8d1
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-12-11 11:44:21 -0500

    -

--- DuelNockles/src/sortomania/contestants/JustinSunny.java
+++ DuelNockles/src/sortomania/contestants/JustinSunny.java
@@ -1,6 +1,7 @@
 package sortomania.contestants;
 
 import java.awt.Color;
+import java.util.Arrays;
 
 import sortomania.Contestant;
 
@@ -16,12 +17,13 @@ public class JustinSunny {
 
 		long duration = (endTime - startTime);
 		System.out.println(duration + " " + mostlySortAndGetMedian(arr));
+		// System.out.println(Arrays.toString(arr));
 	}
 	
 	public static void populate(int[] arr) {
 		/*
 		for(int i = 0; i < arr.length; i++) {
-			if(Math.random() > 0.75)
+			if(Math.random() > 0.25)
 			{
 				arr[i] = i;
 			}
@@ -46,7 +48,7 @@ public class JustinSunny {
 	}
 
 	public static double sortAndGetMedian(int[] random) {
-		sort(random);
+		sort(random, 0, random.length - 1);
 		int halfWayMarker = (int) random.length/2;
 		return (random.length % 2 != 0)?((random[halfWayMarker] + random[halfWayMarker + 1])/2):random[halfWayMarker];
 	}
@@ -240,6 +242,80 @@ public class JustinSunny {
  
         // We reach here when element is not present in array
         return -1;
+    }
+    
+    public static void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+ 
+        /* Create temp arrays */
+        int L[] = new int [n1];
+        int R[] = new int [n2];
+ 
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i)
+            L[i] = arr[l + i];
+        for (int j=0; j<n2; ++j)
+            R[j] = arr[m + 1+ j];
+ 
+ 
+        /* Merge the temp arrays */
+ 
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+ 
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+ 
+        /* Copy remaining elements of L[] if any */
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+        /* Copy remaining elements of R[] if any */
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+ 
+    // Main function that sorts arr[l..r] using
+    // merge()
+    public static void sort(int arr[], int l, int r)
+    {
+        if (l < r)
+        {
+            // Find the middle point
+            int m = (l+r)/2;
+ 
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr , m+1, r);
+ 
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
     }
     
 }
