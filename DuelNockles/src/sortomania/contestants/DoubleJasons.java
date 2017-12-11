26abcf2e0a6db4ebd138a69386b192e21dac004f
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2017-12-11 15:28:54 -0500

    updates

--- DuelNockles/src/sortomania/contestants/DoubleJasons.java
+++ DuelNockles/src/sortomania/contestants/DoubleJasons.java
@@ -1,10 +1,31 @@
 package sortomania.contestants;
 
 public class DoubleJasons {
-
-	public void shellSort(String[] arr) {
-		
+	
+	private int[] ISSeq;
+	
+	public DoubleJasons() {
+		int[] temp = {1, 3, 7, 21, 48, 112, 336, 861, 1968, 4592};
+		ISSeq = temp;
+	}
+	
+	private void shellSort(String[] arr) {
+		int index = 0;
+		while() {
+			
+		}
 		
 	}
 	
+	private void shellSort(int[] random) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+	public int taskOne(int[] random) {
+		shellSort(random);
+		return random[random.length/2];
+	}
+
+
 }
