0acdd7cb325af05d03f53e9e19e393bd0063421b
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-12-11 15:28:57 -0500

    okk

--- DuelNockles/src/sortomania/contestants/JohnsonWeiContestant.java
+++ DuelNockles/src/sortomania/contestants/JohnsonWeiContestant.java
@@ -24,9 +24,47 @@ public class JohnsonWeiContestant extends Contestant {
 
 	@Override
 	public double sortAndGetMedian(int[] random) {
-		// TODO Auto-generated method stub
-		return 0;
+		int last = random.length -1;
+		int max = random.length;
+		quickSort(random, 0, last);
+		
+		if(last % 2 == 0) {
+			return (random[max/2] + random[(max/2)-1]) / 2;
+		}else return random[(max-1)/2];
 	}
+	
+	private void quickSort(int[] array, int lowerIndex, int higherIndex) {
+		int i = lowerIndex;
+        int j = higherIndex;
+        
+        int pivot = array[lowerIndex+(higherIndex-lowerIndex)/2];
+        
+        while (i <= j) {
+            
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+            	int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+                //move index to next position on both sides
+                i++;
+                j--;
+            }
+        }
+        // call quickSort() method recursively
+        if (lowerIndex < j)
+            quickSort(array,lowerIndex, j);
+        if (i < higherIndex)
+            quickSort(array,i, higherIndex);
+        
+        
+    }
+	
 
 	@Override
 	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
@@ -36,8 +74,22 @@ public class JohnsonWeiContestant extends Contestant {
 
 	@Override
 	public double mostlySortAndGetMedian(int[] mostlySorted) {
-		// TODO Auto-generated method stub
-		return 0;
+		int n = mostlySorted.length;  
+		int temp = 0;
+		
+		for(int i=0; i < n; i++){  
+			for(int j=1; j < (n-i); j++){  
+	            if(mostlySorted[j-1] > mostlySorted[j]){  
+		            temp = mostlySorted[j-1];  
+	                mostlySorted[j-1] = mostlySorted[j];  
+	                mostlySorted[j] = temp;  
+	            }  
+            }  
+		}
+		
+		if(n % 2 == 0) {
+			return (mostlySorted[n/2] + mostlySorted[(n/2)-1]) / 2;
+		}else return mostlySorted[(n-1)/2];
 	}
 
 	@Override
@@ -48,8 +100,7 @@ public class JohnsonWeiContestant extends Contestant {
 
 	@Override
 	public int sortAndSearch(Comparable[] arr, Comparable toFind) {
-		// TODO Auto-generated method stub
-		return 0;
+		
 	}
 
 }
