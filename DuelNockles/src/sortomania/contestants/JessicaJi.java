076f3fda4f1154617e732b7a9ae1ed323c60a07d
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-12-11 15:04:43 -0500

    method

--- DuelNockles/src/sortomania/contestants/JessicaJi.java
+++ DuelNockles/src/sortomania/contestants/JessicaJi.java
@@ -6,10 +6,30 @@ import sortomania.Contestant;
 
 public class JessicaJi extends Contestant {
 
+	public static void main(String[] args) {
+		JessicaJi test = new JessicaJi();
+		int[] arr = {4, 7, 10, 2, 8, 12, 100, 34, 42, 23, 40, 56, 31, 8};
+		System.out.println("The median is: " + test.sortAndGetMedian(arr));
+		System.out.println("And the sorted array is: \n" + insertionSort(arr));
+	}
+	public static int[] insertionSort(int[] arr) {
+		// TODO Auto-generated method stub
+		return arr;
+	}
+	
+	public static int[] mergeSort(int[] arr) {
+		// TODO Auto-generated method stub
+		return arr;
+	}
+	
+	public static String[] insertionSortString(String[] arr) {
+		// TODO Auto-generated method stub
+		return arr;
+	}
 	public JessicaJi() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
 	@Override
 	public Color getColor() {
 		return new Color(174, 198, 208);
@@ -47,11 +67,6 @@ public class JessicaJi extends Contestant {
 	public int sortAndSearch(Comparable[] arr, Comparable toFind) {
 		// TODO Auto-generated method stub
 		return 0;
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 	
 	@Override
