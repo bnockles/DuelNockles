9fa1f98e87657529375e574b01283eccf93d68c1
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-12-11 10:45:43 -0500

    a

---
+++ DuelNockles/src/sortomania/CarsonSort.java
@@ -0,0 +1,49 @@
+package sortomania;
+
+import java.awt.Color;
+
+public class CarsonSort extends Contestant{
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSpriteName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double sortAndGetMedian(int[] random) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double mostlySortAndGetMedian(int[] mostlySorted) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double sortMultiDim(int[][] grid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sortAndSearch(Comparable[] arr, Comparable toFind) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
+
