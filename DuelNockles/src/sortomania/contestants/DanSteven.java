3f158c32b55c5eb3af9242eabf9bc30976ce94e8
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-12-11 10:38:51 -0500

    a

---
+++ DuelNockles/src/sortomania/contestants/DanSteven.java
@@ -0,0 +1,44 @@
+package sortomania.contestants;
+
+import java.awt.Color;
+
+import sortomania.Contestant;
+
+public class DanSteven extends Contestant{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getSpriteName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public double mostlySortAndGetMedian(int[] mostlySorted) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public double sortMultiDim(int[][] grid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int sortAndSearch(Comparable[] arr, Comparable toFind) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
