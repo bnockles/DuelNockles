ea41a8a5d4ba7950a65f4f4c005a26fd1b4b2b45
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-12-11 11:36:10 -0500

    fixed

--- DuelNockles/src/sortomania/contestants/YonathanDavid.java
+++ DuelNockles/src/sortomania/contestants/YonathanDavid.java
@@ -73,44 +73,6 @@ public class YonathanDavid extends Contestant{
 		//Yonathan
 		return 0;
 	}
-	//http://www.java2novice.com/java-sorting-algorithms/merge-sort/
-    private void doMergeSort(int lowerIndex, int higherIndex) {
-        
-        if (lowerIndex < higherIndex) {
-            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
-            // Below step sorts the left side of the array
-            doMergeSort(lowerIndex, middle);
-            // Below step sorts the right side of the array
-            doMergeSort(middle + 1, higherIndex);
-            // Now merge both sides
-            mergeParts(lowerIndex, middle, higherIndex);
-        }
-    }
- 
-    private void mergeParts(int lowerIndex, int middle, int higherIndex) {
- 
-        for (int i = lowerIndex; i <= higherIndex; i++) {
-            tempMergArr[i] = array[i];
-        }
-        int i = lowerIndex;
-        int j = middle + 1;
-        int k = lowerIndex;
-        while (i <= middle && j <= higherIndex) {
-            if (tempMergArr[i] <= tempMergArr[j]) {
-                array[k] = tempMergArr[i];
-                i++;
-            } else {
-                array[k] = tempMergArr[j];
-                j++;
-            }
-            k++;
-        }
-        while (i <= middle) {
-            array[k] = tempMergArr[i];
-            k++;
-            i++;
-        }
- 
-    }
+	
 
 }
