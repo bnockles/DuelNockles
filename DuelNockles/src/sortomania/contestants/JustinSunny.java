77f4e6f1955bca4f4019851763160994c007100c
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-12-11 11:08:33 -0500

    -

--- DuelNockles/src/sortomania/contestants/JustinSunny.java
+++ DuelNockles/src/sortomania/contestants/JustinSunny.java
@@ -27,7 +27,8 @@ public class JustinSunny extends Contestant {
 
 	@Override
 	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
-		return 0;
+		sort(strings);
+		return binarySearch(strings, 0, strings.length - 1, toFind);
 	}
 
 	@Override
@@ -73,9 +74,30 @@ public class JustinSunny extends Contestant {
         }
     }
 	
+	public void sort(String arr[])
+    {
+        int n = arr.length;
+ 
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+ 
+        // One by one extract an element from heap
+        for (int i=n-1; i>=0; i--)
+        {
+            // Move current root to end
+            String temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+ 
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+	
     // To heapify a subtree rooted with node i which is
     // an index in arr[]. n is size of heap
-    void heapify(int arr[], int n, int i)
+    public void heapify(int arr[], int n, int i)
     {
         int largest = i;  // Initialize largest as root
         int l = 2*i + 1;  // left = 2*i + 1
@@ -100,5 +122,80 @@ public class JustinSunny extends Contestant {
             heapify(arr, n, largest);
         }
     }
+    
+    public void heapify(String arr[], int n, int i)
+    {
+        int largest = i;  // Initialize largest as root
+        int l = 2*i + 1;  // left = 2*i + 1
+        int r = 2*i + 2;  // right = 2*i + 2
+ 
+        // If left child is larger than root
+        if (l < n && arr[l].compareTo(arr[largest]) > 0)
+            largest = l;
+ 
+        // If right child is larger than largest so far
+        if (r < n && arr[r].compareTo(arr[largest]) > 0)
+            largest = r;
+ 
+        // If largest is not root
+        if (largest != i)
+        {
+            String swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+ 
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
 	
+    public int binarySearch(int arr[], int l, int r, int x)
+    {
+        if (r>=l)
+        {
+            int mid = l + (r - l)/2;
+ 
+            // If the element is present at the middle itself
+            if (arr[mid] == x)
+               return mid;
+ 
+            // If element is smaller than mid, then it can only
+            // be present in left subarray
+            if (arr[mid] > x)
+               return binarySearch(arr, l, mid-1, x);
+ 
+            // Else the element can only be present in right
+            // subarray
+            return binarySearch(arr, mid+1, r, x);
+        }
+ 
+        // We reach here when element is not present in array
+        return -1;
+    }
+    
+    public int binarySearch(String arr[], int l, int r, String x)
+    {
+        if (r>=l)
+        {
+            int mid = l + (r - l)/2;
+            int compareValue = arr[mid].compareTo(x);
+            
+            // If the element is present at the middle itself
+            if (compareValue == 0)
+               return mid;
+ 
+            // If element is smaller than mid, then it can only
+            // be present in left subarray
+            if (compareValue > 0)
+               return binarySearch(arr, l, mid-1, x);
+ 
+            // Else the element can only be present in right
+            // subarray
+            return binarySearch(arr, mid+1, r, x);
+        }
+ 
+        // We reach here when element is not present in array
+        return -1;
+    }
+    
 }
