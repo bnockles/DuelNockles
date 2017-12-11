ec82e2c904d97a3e8ba4c60ac3b23a380e32b56b
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2017-12-11 11:41:01 -0500

    sdfsdfsdfsdf

--- DuelNockles/src/sortomania/SidBen.java
+++ DuelNockles/src/sortomania/SidBen.java
@@ -7,10 +7,10 @@ public class SidBen extends Contestant{
 	private int[] numbers;
 	private int number;
 	
-	public static void main(String[] args) {
+	public static void dort() {
 		int[] temp = {5,1,4,7,3,3};
-		
-		sort(temp);
+		MyQuickSort s = new MyQuickSort();
+		s.sort(temp);
 	}
 	
 	
@@ -21,23 +21,29 @@ public class SidBen extends Contestant{
 	}
 	
 	
-	
-	
-	
-	
-	
 	@Override
 	public String getSpriteName() {
 		// TODO Auto-generated method stub
 		return DEE_JAY;
 	}
 
+	
 	@Override
 	public double sortAndGetMedian(int[] random) {
-		// TODO Auto-generated method stub
-		return 0;
+		MyQuickSort s = new MyQuickSort();
+		s.sort(random);
+		
+		int l = random.length;
+		
+		if(l %2 == 0) {
+			return (random[l/2] + random[l+1/2])/2;
+		}else {
+			return random[l/2];
+		}
+		
 	}
 
+	
 	@Override
 	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
 		// TODO Auto-generated method stub
@@ -64,56 +70,8 @@ public class SidBen extends Contestant{
 	
 	
 	//------------------------------------------------------------------------------------
-	 public static void sort(int[] values) {
-		 	
-	        // check for empty or null array
-	        if (values ==null || values.length==0){
-	            return;
-	        }
-	        this.numbers = values;
-	        number = values.length;
-	        quicksort(0, number - 1);
-	    }
-
-	    private static void quicksort(int low, int high) {
-	        int i = low, j = high;
-	        // Get the pivot element from the middle of the list
-	        int pivot = numbers[low + (high-low)/2];
-
-	        // Divide into two lists
-	        while (i <= j) {
-	            // If the current value from the left list is smaller than the pivot
-	            // element then get the next element from the left list
-	            while (numbers[i] < pivot) {
-	                i++;
-	            }
-	            // If the current value from the right list is larger than the pivot
-	            // element then get the next element from the right list
-	            while (numbers[j] > pivot) {
-	                j--;
-	            }
-
-	            // If we have found a value in the left list which is larger than
-	            // the pivot element and if we have found a value in the right list
-	            // which is smaller than the pivot element then we exchange the
-	            // values.
-	            // As we are done we can increase i and j
-	            if (i <= j) {
-	                exchange(i, j);
-	                i++;
-	                j--;
-	            }
-	        }
-	        // Recursion
-	        if (low < j)
-	            quicksort(low, j);
-	        if (i < high)
-	            quicksort(i, high);
-	    }
-
-	    private static void exchange(int i, int j) {
-	        int temp = numbers[i];
-	        numbers[i] = numbers[j];
-	        numbers[j] = temp;
-	    }
+	
+	
+	
+	
 }
