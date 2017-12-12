9dbb23e9b65b368486f7f3c87c43749fc521a539
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-12-12 15:25:21 -0500

    aye

--- DuelNockles/src/sortomania/contestants/AbidAbedContestant.java
+++ DuelNockles/src/sortomania/contestants/AbidAbedContestant.java
@@ -1,12 +1,27 @@
 package sortomania.contestants;
 
 import java.awt.Color;
+import java.util.Arrays;
 
 import sortomania.Contestant;
 
 public class AbidAbedContestant extends Contestant
 {
 
+	public static void main(String[] args)
+	{
+		AbidAbedContestant ab = new AbidAbedContestant();
+		int[] x = { 9, 2, 4, 7, 3, 7, 10 };
+		System.out.println(Arrays.toString(x));
+ 
+		int low = 0;
+		int high = x.length - 1;
+ 
+		ab.insertionSort(x);
+		System.out.println(Arrays.toString(x));
+		System.out.println(ab.mostlySortAndGetMedian(x));
+		
+	}
 	@Override
 	public Color getColor() 
 	{
@@ -22,7 +37,8 @@ public class AbidAbedContestant extends Contestant
 	@Override
 	public double sortAndGetMedian(int[] random) 
 	{
-		quickSort(0,random.length-1,random);
+		quickSort(random,0,random.length-1);
+		return median(random);
 		
 	}
 
@@ -36,8 +52,8 @@ public class AbidAbedContestant extends Contestant
 	@Override
 	public double mostlySortAndGetMedian(int[] mostlySorted) 
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		insertionSort(mostlySorted);
+		return median(mostlySorted);
 	}
 
 	@Override
@@ -52,53 +68,81 @@ public class AbidAbedContestant extends Contestant
 	{
 		// TODO Auto-generated method stub
 		return 0;
-	}
-	private void quickSort(int low, int high,int[] numbers) 
+	} 
+	public void insertionSort(int array[]) 
 	{
-        int i = high, j = low;
-       
-        int pivot = numbers[low + (high-low)/2];
-
-        
-        while (i <= j) 
+        int n = array.length;
+        for (int j = 1; j < n; j++) 
         {
-       
-            while (numbers[i] < pivot) 
+            int key = array[j];
+            int i = j-1;
+            while ( (i > -1) && ( array [i] > key ) ) 
             {
-                i++;
+                array [i+1] = array [i];
+                i--;
             }
-          
-            while (numbers[j] > pivot) 
-            {
-                j--;
-            }
-
-            if (i <= j) {
-                exchange(i, j,numbers);
-                i++;
-                j--;
-            }
+            array[i+1] = key;
+            
         }
-      
-        if (low < j)
-            quickSort(low, j,numbers);
-        if (i < high)
-            quickSort(i, high,numbers);
     }
-
-    private void exchange(int i, int j,int[] numbers) 
+	public  void quickSort(int[] arr, int low, int high) 
+	{
+		
+		if (low >= high)
+			return;
+ 
+		// pick the pivot
+		int middle = low + (high - low) / 2;
+		int pivot = arr[middle];
+ 
+		// make left < pivot and right > pivot
+		int i = low;
+		int j = high;
+		while (i <= j) 
+		{
+			while (arr[i] < pivot) 
+			{
+				i++;
+			}
+ 
+			while (arr[j] > pivot)
+			{
+				j--;
+			}
+ 
+			if (i <= j) 
+			{
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				i++;
+				j--;
+			}
+		}
+ 
+		// recursively sort two sub parts
+		if (low < j)
+			quickSort(arr, low, j);
+ 
+		if (high > i)
+			quickSort(arr, i, high);
+	}	
+    
+    private  double median(int[] random)
     {
-        int temp = numbers[i];
-        numbers[i] = numbers[j];
-        numbers[j] = temp;
-    }
-    private double median(int[] numbers)
-    {
-    	if(numbers.length % 2 == 0)
-    	{
-    		
-    	}
-		return ;
+    	if(random.length % 2 == 1)
+		{
+			return random[(random.length-1)/2];
+		}
+		else if(random.length % 2 == 0)
+		{
+			int x = (random.length)/2;
+			return (double)(random[x] + random[x-1])/2;
+		}
+		else
+		{
+			return -1;
+		}
     	
     }
 
