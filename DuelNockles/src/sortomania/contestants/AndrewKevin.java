1ef6255e18ee4cdbb524b34bcd25b86d8fb5834f
Andrew <az@192.168.1.144>
2017-12-12 17:53:10 -0500

    12-12

--- DuelNockles/src/sortomania/contestants/AndrewKevin.java
+++ DuelNockles/src/sortomania/contestants/AndrewKevin.java
@@ -25,49 +25,49 @@ public class AndrewKevin extends Contestant{
 	public   double sortAndGetMedian(int[] random) {
 		sort(random);
 		if(random.length%2 ==0) {
-			return ((random[random.length/2]+random[(random.length/2)+1])/2);
+			return (double)(random[random.length/2-1]+random[random.length/2])/2;
 		}
-		return random[(int)((random.length/2))];
+		return (double)random[((random.length/2))];
 	}
-	 public void sort(int[] inputArr) {
-         
-	        if (inputArr == null || inputArr.length == 0) {
-	            return;
-	        }
-	        this.array = inputArr;
-	        length = inputArr.length;
-	        quickSort(0, length - 1);
-	    }
 	
-	private void quickSort(int lowerIndex, int higherIndex) {
-        
-        int i = lowerIndex;
-        int j = higherIndex;
-        int pivot = array[lowerIndex+(higherIndex-lowerIndex)/2];
-        while (i <= j) {
-            while (array[i] < pivot) {
-                i++;
-            }
-            while (array[j] > pivot) {
-                j--;
-            }
-            if (i <= j) {
-                exchangeNumbers(i, j);
-                i++;
-                j--;
-            }
-        }
-        if (lowerIndex < j)
-            quickSort(lowerIndex, j);
-        if (i < higherIndex)
-            quickSort(i, higherIndex);
-    }
+		 	public void sort(int[] inputArr) {
+	         
+		        if (inputArr == null || inputArr.length == 0) {
+		            return;
+		        }
+		        this.array = inputArr;
+		        length = inputArr.length;
+		        quickSort(0, length - 1);
+		    }
+			private void quickSort(int lowerIndex, int higherIndex) {
+		        
+		        int i = lowerIndex;
+		        int j = higherIndex;
+		        int pivot = array[lowerIndex+(higherIndex-lowerIndex)/2];
+		        while (i <= j) {
+		            while (array[i] < pivot) {
+		                i++;
+		            }
+		            while (array[j] > pivot) {
+		                j--;
+		            }
+		            if (i <= j) {
+		                exchangeNumbers(i, j);
+		                i++;
+		                j--;
+		            }
+		        }
+		        if (lowerIndex < j)
+		            quickSort(lowerIndex, j);
+		        if (i < higherIndex)
+		            quickSort(i, higherIndex);
+		    }
  
-    private void exchangeNumbers(int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
+		    private void exchangeNumbers(int i, int j) {
+		        int temp = array[i];
+		        array[i] = array[j];
+		        array[j] = temp;
+		    }
   /**------------------------------------------**/
 	@Override
 	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
@@ -80,38 +80,38 @@ public class AndrewKevin extends Contestant{
 		return -1;
 	}
 	
-    public static void mergeSort(String[] names) {
-        if (names.length >= 2) {
-            String[] left = new String[names.length / 2];
-            String[] right = new String[names.length - names.length / 2];
-
-            for (int i = 0; i < left.length; i++) {
-                left[i] = names[i];
-            }
-
-            for (int i = 0; i < right.length; i++) {
-                right[i] = names[i + names.length / 2];
-            }
-
-            mergeSort(left);
-            mergeSort(right);
-            merge(names, left, right);
-        }
-    }
-
-    public static void merge(String[] names, String[] left, String[] right) {
-        int a = 0;
-        int b = 0;
-        for (int i = 0; i < names.length; i++) {
-            if (b >= right.length || (a < left.length && left[a].compareToIgnoreCase(right[b]) < 0)) {
-                names[i] = left[a];
-                a++;
-            } else {
-                names[i] = right[b];
-                b++;
-            }
-        }
-    }
+		    public static void mergeSort(String[] names) {
+		        if (names.length >= 2) {
+		            String[] left = new String[names.length / 2];
+		            String[] right = new String[names.length - names.length / 2];
+		
+		            for (int i = 0; i < left.length; i++) {
+		                left[i] = names[i];
+		            }
+		
+		            for (int i = 0; i < right.length; i++) {
+		                right[i] = names[i + names.length / 2];
+		            }
+		
+		            mergeSort(left);
+		            mergeSort(right);
+		            merge(names, left, right);
+		        }
+		    }
+		
+		    public static void merge(String[] names, String[] left, String[] right) {
+		        int a = 0;
+		        int b = 0;
+		        for (int i = 0; i < names.length; i++) {
+		            if (b >= right.length || (a < left.length && left[a].compareToIgnoreCase(right[b]) < 0)) {
+		                names[i] = left[a];
+		                a++;
+		            } else {
+		                names[i] = right[b];
+		                b++;
+		            }
+		        }
+		    }
     /**------------------------------------------**/
 	@Override
 	public double mostlySortAndGetMedian(int[] mostlySorted) {
@@ -127,7 +127,11 @@ public class AndrewKevin extends Contestant{
 	            }
 	            mostlySorted[j+1] = key;
 	        }
-		return 0;
+	        
+        if(mostlySorted.length%2 ==0) {
+			return (double)(mostlySorted[mostlySorted.length/2-1]+mostlySorted[mostlySorted.length/2])/2;
+		}
+		return (double)mostlySorted[((mostlySorted.length/2))];
 	}
 	 /**------------------------------------------**/
 	@Override
--- DuelNockles/src/sortomania/contestants/KevinAndrewTest.java
+++ DuelNockles/src/sortomania/contestants/KevinAndrewTest.java
@@ -11,7 +11,7 @@ public static void main(String a[]){
         //for(int i = 0; i < input.length; i++) {
         //	input[i] = (int) (Math.random()*10001);
         //}
-        int[] input = {24,2,45,20,56,75,2,56,53,12};
+        int[] input = {10,9,8,7,6,5,4,3,2};
         final long startTime = System.nanoTime();
         sorter.mostlySortAndGetMedian(input);
         final long duration = System.nanoTime() - startTime;
@@ -19,7 +19,7 @@ public static void main(String a[]){
             System.out.println(i);
         }
         System.out.println ("\n" + duration + " nanoseconds");
-        System.out.println(sorter.sortAndGetMedian(input));
+        System.out.println(sorter.mostlySortAndGetMedian(input));
         
         String[] fruits = {"Apple","Mango","Peach","Banana","Orange","aaaaa","Grapes","Watermelon","Tomato"};
         final long startTime2 = System.nanoTime();
