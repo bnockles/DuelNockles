f2a4e245ea5a0312611e0a69ef9c4923e045e1da
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2017-12-12 11:24:41 -0500

    b

--- DuelNockles/src/sortomania/contestants/DanSteven.java
+++ DuelNockles/src/sortomania/contestants/DanSteven.java
@@ -37,8 +37,19 @@ public class DanSteven extends Contestant{
 		return "Steven";
 	}
 	@Override
-	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
-		return 0;
+	public int sortAndGetResultingIndexOf(String[] strings, String toFind)
+	{
+		mergeSort(strings);
+		
+		for (int i = 0; i < strings.length; i++)
+		{
+			if (strings[i].equals(toFind))
+			{
+				return i;
+			}
+		}
+		
+		return -1;
 	}
 	@Override
 	public double mostlySortAndGetMedian(int[] mostlySorted) {
@@ -121,4 +132,39 @@ public class DanSteven extends Contestant{
 	         countSort(arr, n, exp);
 	 }
 
+	public void mergeSort(String[] a) {
+        if (a.length >= 2) {
+            String[] left = new String[a.length / 2];
+            String[] right = new String[a.length-a.length / 2];
+
+            for (int i = 0; i < left.length; i++)
+            {
+                left[i] = a[i];
+            }
+            for (int i = 0; i < right.length; i++)
+            {
+                right[i] = a[i + a.length / 2];
+            }
+
+            mergeSort(left);
+            mergeSort(right);
+
+            merge(a, left, right);
+        }
+    }
+
+    public void merge(String[] result, String[] left, String[] right) {
+        int i1 = 0;
+        int i2 = 0;
+        for (int i = 0; i < result.length; i++) {
+        	 if (i2 >= right.length || (i1 < left.length &&
+                     left[i1].compareToIgnoreCase(right[i2])<0)) {
+                      result[i] = left[i1];
+                      i1++;
+                 } else {
+                      result[i] = right[i2];
+                      i2++;
+                 }
+            }
+    }
 }
