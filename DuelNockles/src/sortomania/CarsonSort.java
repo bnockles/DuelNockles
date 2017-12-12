9a74016545bdd39ef1ef1916d57dfb16c7dd5ee7
TylerOvenden <tovenden4386@bths.edu>
2017-12-12 10:54:46 -0500

    Update CarsonSort.java

--- DuelNockles/src/sortomania/CarsonSort.java
+++ DuelNockles/src/sortomania/CarsonSort.java
@@ -9,9 +9,10 @@ public class CarsonSort extends Contestant{
 	private int length;
 
 	public static void main(String a[]){
-		int[] input = {24,2,45,20,56,75,2,56,99,53,12};
+		CarsonSort test = new CarsonSort();
+		int[] input = {24,2,45,20,56,75,2,56,99,53,12,1};
 		CarsonSort.quickSort(input, 0, input.length - 1);
-		sortAndGetMedian(input);
+		System.out.println("The median is: " + test.sortAndGetMedian(input));
 		for(int i:input){
             System.out.print(i);
             System.out.print(" ");
@@ -21,48 +22,58 @@ public class CarsonSort extends Contestant{
 	public Color getColor() {
 		return new Color(123,123,123);
 	}
-
+//1,2,3,4 2+3/2 
 	public String getSpriteName() {
 		return "CarsonTylerSprite";
 	}
 
-	public static double sortAndGetMedian(int[] random) {
+	public double sortAndGetMedian(int[] random) {
 		quickSort(random, 0, random.length-1);
-		System.out.println(Arrays.toString(random));
+		if(random.length % 2 == 0) {
+			return (random[(int)random.length /2]+random[((int)random.length /2)+1])/2;
+		}
+		else
 		return random[(int)random.length /2];
+		//System.out.println(Arrays.toString(random));
+
 	}
 
 	@Override
 	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
-		boolean done = false;
-		while(!done)
+		return 0;
+	}
+	void insertSort(int arr[])
+	{
+		int n = arr.length;
+		for (int i=1; i<n; ++i)
 		{
-		    done=true;   
-		    for(int i=0;i<organizedNames.length-1;i++)
-		    {
-		        if(!(organizednames[i]==null))
-		        {
-		            String name1=organizedNames[i]; String name2=organizedNames[i+1];
-		            if(name1!=null&&name2!=null)
-		            {
-		                int num=name1.compareTo(name2);
-		                if(num>0)
-		                { 
-		                    temp=organizedNames[i]; //temp is a String that was declared earlier
-		                    organizedNames[i]=organizedNames[i+1];
-		                    organizedNames[i+1]=temp;
-		                    done=false 
-		                }
-		            }
-		        }
-		    }
+			int key = arr[i];
+			int j = i-1;
+
+			/* Move elements of arr[0..i-1], that are
+			greater than key, to one position ahead
+			of their current position */
+			while (j>=0 && arr[j] > key)
+			{
+				arr[j+1] = arr[j];
+				j = j-1;
+			}
+			arr[j+1] = key;
+		    int m = arr.length;
+		       for (int l=0; l<m; ++l)
+		    	   System.out.print(arr[i] + " ");
+		 
+		        System.out.println();
 		}
 	}
 
 	@Override
 	public double mostlySortAndGetMedian(int[] mostlySorted) {
-		// TODO Auto-generated method stub
-		return 0;
+		insertSort(mostlySorted);
+		if(mostlySorted.length % 2 == 0) {
+			return (mostlySorted[(int)mostlySorted.length /2]+mostlySorted[((int)mostlySorted.length /2)+1])/2;
+		}
+		return mostlySorted[(int)mostlySorted.length /2];
 	}
 
 	@Override
@@ -86,7 +97,7 @@ public class CarsonSort extends Contestant{
 		int i = lowerIndex;
 		int j = higherIndex;
 		// calculate pivot number, I am taking pivot as middle index number
-		int pivot = array[lowerIndex+(higherIndex-lowerIndex)/2];
+		int pivot = intArr[lowerIndex+(higherIndex-lowerIndex)/2];
 		// Divide into two arrays
 		while (i <= j) {
 			/**
@@ -95,14 +106,14 @@ public class CarsonSort extends Contestant{
 			 * from right side which is less then the pivot value. Once the search 
 			 * is done, then we exchange both numbers.
 			 */
-			while (array[i] < pivot) {
+			while (intArr[i] < pivot) {
 				i++;
 			}
-			while (array[j] > pivot) {
+			while (intArr[j] > pivot) {
 				j--;
 			}
 			if (i <= j) {
-				swap(i, j);
+				swap(intArr, i, j);
 				//move index to next position on both sides
 				i++;
 				j--;
@@ -140,7 +151,7 @@ public class CarsonSort extends Contestant{
 				j--;
 			}
 			if (i <= j) {
-				swap(i, j);
+				swap(strArr, i, j);
 				//move index to next position on both sides
 				i++;
 				j--;
@@ -153,10 +164,10 @@ public class CarsonSort extends Contestant{
 			quickSort(strArr, i, higherIndex);
 	}
 
-	private static void swap(int i, int j) {
-		int temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
+	private static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 }
 
