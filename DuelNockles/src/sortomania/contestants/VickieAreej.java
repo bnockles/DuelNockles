402f3287863ad5f8fce59fa907151b9afb113c62
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-12-12 15:26:10 -0500

    sdfsdf

--- DuelNockles/src/sortomania/contestants/VickieAreej.java
+++ DuelNockles/src/sortomania/contestants/VickieAreej.java
@@ -14,6 +14,8 @@ public class VickieAreej extends Contestant{
 
 	public static void main(String[] args) {
 		int[] arr = {4, 7,10, 2,18,12, 34, 42, 23, 40, 56, 31, 8, 56,234,56,35,234,67};
+		int[][] arr2 = {{2,1,3},{23,54,23}};
+		multiArraySort(arr2);
 		heapSort(arr);
 		//bubbleSort(arr);
 		System.out.print(Arrays.toString(arr));
@@ -34,34 +36,24 @@ public class VickieAreej extends Contestant{
 		public static void heapSort(int arr[])
 	    {
 	        int n = arr.length;
-	 
-	        // Build heap (rearrange array)
 	        for (int i = n / 2 - 1; i >= 0; i--)
 	            heapify(arr, n, i);
-	 
-	        // One by one extract an element from heap
 	        for (int i=n-1; i>=0; i--)
 	        {
-	            // Move current root to end
 	            int temp = arr[0];
 	            arr[0] = arr[i];
 	            arr[i] = temp;
 	 
-	            // call max heapify on the reduced heap
 	            heapify(arr, i, 0);
 	        }
-	        //return arr;
 	    }
 	 
-	    // To heapify a subtree rooted with node i which is
-	    // an index in arr[]. n is size of heap
-	    static void heapify(int arr[], int n, int i)
+	    public static void heapify(int arr[], int n, int i)
 	    {
 	        int largest = i;  // Initialize largest as root
 	        int l = 2*i + 1;  // left = 2*i + 1
 	        int r = 2*i + 2;  // right = 2*i + 2
-	 
-	        // If left child is larger than root
+	
 	        if (l < n && arr[l] > arr[largest])
 	            largest = l;
 	 
@@ -96,7 +88,7 @@ public class VickieAreej extends Contestant{
 		heapSort(random);
 		//get median
 		if(random.length%2 ==0) {
-			median = (random[random.length/2]+random[(random.length/2)+1])/2;
+			median = (random[random.length/2]+random[(random.length/2)-1])/2;
 		}else {
 			median = random[(int) ((random.length/2)+.5)];
 		}
@@ -131,7 +123,7 @@ public class VickieAreej extends Contestant{
 		bubbleSort(mostlySorted);
 		//get median
 		if(mostlySorted.length%2 ==0) {
-			median = (mostlySorted[mostlySorted.length/2]+mostlySorted[(mostlySorted.length/2)+1])/2;
+			median = (mostlySorted[mostlySorted.length/2]+mostlySorted[(mostlySorted.length/2)-1])/2;
 		}else {
 			median = mostlySorted[(int) ((mostlySorted.length/2)+.5)];
 		}
@@ -140,15 +132,80 @@ public class VickieAreej extends Contestant{
 
 	@Override
 	public double sortMultiDim(int[][] grid) {
-		// TODO Auto-generated method stub
+		multiArraySort(grid);
 		return 0;
 	}
+
+	private static void multiArraySort(int[][] grid) {
+		//int A[][]=new int[m][n]; //creating a 2D array
+        int m = grid.length;
+        int n = grid[m].length;
+        /* Inputting the 2D Array */
+
+       /* for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print("Enter the elements: ");
+                A[i][j]=Integer.parseInt(br.readLine());
+            }
+        }    */    
+ 
+        /* Printing the original 2D Array */
+
+        System.out.println("The original array:");
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(grid[i][j]+"\t");
+            }
+            System.out.println();
+        }
+
+        /* Sorting the 2D Array */
+
+        int t=0;
+        for(int x=0;x<m;x++)
+        {
+            for(int y=0;y<n;y++)
+            {
+                for(int i=0;i<m;i++)
+                {
+                    for(int j=0;j<n;j++)
+                    {
+                        if(grid[i][j]>grid[x][y])
+                        {
+                            t=grid[x][y];
+                            grid[x][y]=grid[i][j];
+                            grid[i][j]=t;
+                        }
+                    }
+                }
+            }
+        }
+
+        /* Printing the sorted 2D Array */
+
+        System.out.println("The Sorted Array:");
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(grid[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+
+	
 
 	@Override
 	public int sortAndSearch(Comparable[] arr, Comparable toFind) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
 	public String toString() {
 		return "A & V";
 		
