8c288147204ae32fd36ff086179e34b60cf2f29a
TylerOvenden <tovenden4386@bths.edu>
2017-12-12 11:26:24 -0500

    Update CarsonSort.java

--- DuelNockles/src/sortomania/CarsonSort.java
+++ DuelNockles/src/sortomania/CarsonSort.java
@@ -43,33 +43,28 @@ public class CarsonSort extends Contestant{
 	}
 	void insertSort(int arr[])
 	{
-		int n = arr.length;
-		for (int i=1; i<n; ++i)
-		{
-			int key = arr[i];
-			int j = i-1;
-
-			/* Move elements of arr[0..i-1], that are
-			greater than key, to one position ahead
-			of their current position */
-			while (j>=0 && arr[j] > key)
-			{
-				arr[j+1] = arr[j];
-				j = j-1;
-			}
-			arr[j+1] = key;
-		    int m = arr.length;
-		       for (int l=0; l<m; ++l)
-		    	   System.out.print(arr[i] + " ");
+		      int n = arr.length;
+		        for (int i=1; i<n; ++i)
+		        {
+		            int key = arr[i];
+		            int j = i-1;
 		 
-		        System.out.println();
-		}
+		            /* Move elements of arr[0..i-1], that are
+		               greater than key, to one position ahead
+		               of their current position */
+		            while (j>=0 && arr[j] > key)
+		            {
+		                arr[j+1] = arr[j];
+		                j = j-1;
+		            }
+		            arr[j+1] = key;
+		        }
 	}
 
 	@Override
 	public double mostlySortAndGetMedian(int[] mostlySorted) {
 		insertSort(mostlySorted);
-
+	    printArray(mostlySorted);
 		if(mostlySorted.length % 2 == 0) {
 			return (mostlySorted[(int)mostlySorted.length /2]+mostlySorted[((int)mostlySorted.length /2)+1])/2;
 		}
@@ -128,9 +123,23 @@ public class CarsonSort extends Contestant{
 		if (i < higherIndex)
 			quickSort(intArr, i, higherIndex);
 	}
+	
+	static void printArray(int arr[])
+	    {
+	        int n = arr.length;
+	        for (int i=0; i<n; ++i)
+	            System.out.print(arr[i] + " ");
+	 
+	        System.out.println();
+	    }
 
 	private static void swap(int[] arr, int i, int j) {
 		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	private static void swap(String[] arr, int i, int j) {
+		String temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
 	}
