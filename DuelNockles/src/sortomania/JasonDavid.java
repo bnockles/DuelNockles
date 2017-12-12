82f3a231bffb5d6cd46f4473559c624e7aa48711
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-12-12 11:38:23 -0500

    Sort

--- DuelNockles/src/sortomania/JasonDavid.java
+++ DuelNockles/src/sortomania/JasonDavid.java
@@ -3,11 +3,19 @@ package sortomania;
 import java.awt.Color;
 
 public class JasonDavid{ //extends Contestants
+	private int[] numbers;
+    private int[] helper;
+
+    private int number;
+    public int count;
+    public JasonDavid() {
+    	count=0;
+    }
 	public static void main(String args[]) {
 		JasonDavid hi = new JasonDavid();
 		long num = simAvg();
 		System.out.println(num+ " " + simSD(num)+" ms");
-		
+		System.out.println(hi.count);
 	}
 	
 	public static long simAvg() {
@@ -65,7 +73,7 @@ public class JasonDavid{ //extends Contestants
 	}
 	//@Override
 	public double sortAndGetMedian(int[] random) {
-		 sort(random,0,random.length-1);
+		quickSort(random,0,random.length-1);
 		 if (random.length%2==0) {
 			 int floor = (int)Math.floor(random.length/2)-1;
 			 int ceiling = floor+1;
@@ -76,9 +84,15 @@ public class JasonDavid{ //extends Contestants
 		 }
 		
 	}
+	public boolean checkSort(int[] arr) {
+		if(arr[0]<arr[1] && arr[1]<arr[2]&& arr[2]<arr[3]&& arr[4]<arr[5]) {
+			return true;
+		}
+		return false;
+	}
 	int partition(int arr[], int low, int high)
     {
-        int pivot = arr[high]; 
+        int pivot = arr[randInt(low,high)]; 
         int i = (low-1); // index of smaller element
         for (int j=low; j<high; j++)
         {
@@ -106,9 +120,9 @@ public class JasonDavid{ //extends Contestants
     arr[] --> Array to be sorted,
     low  --> Starting index,
     high  --> Ending index */
-	  void sort(int arr[], int low, int high)
-	  {
-	      if (low < high)
+	 
+	  public void quickSort(int arr[], int low, int high) {
+		  if (low < high)
 	      {
 	          /* pi is partitioning index, arr[pi] is 
 	            now at right place */
@@ -116,10 +130,63 @@ public class JasonDavid{ //extends Contestants
 	
 	          // Recursively sort elements before
 	          // partition and after partition
-	          sort(arr, low, pi-1);
-	          sort(arr, pi+1, high);
+	          quickSort(arr, low, pi-1);
+	          quickSort(arr, pi+1, high);
 	      }
 	  }
+	  public void mSort(int[] values) {
+	        this.numbers = values;
+	        number = values.length;
+	        this.helper = new int[number];
+	        mergesort(0, number - 1);
+	    }
+
+	    private void mergesort(int low, int high) {
+	        // check if low is smaller than high, if not then the array is sorted
+	        if (low < high) {
+	            // Get the index of the element which is in the middle
+	            int middle = low + (high - low) / 2;
+	            // Sort the left side of the array
+	            mergesort(low, middle);
+	            // Sort the right side of the array
+	            mergesort(middle + 1, high);
+	            // Combine them both
+	            merge(low, middle, high);
+	        }
+	    }
+
+	    private void merge(int low, int middle, int high) {
+
+	        // Copy both parts into the helper array
+	        for (int i = low; i <= high; i++) {
+	            helper[i] = numbers[i];
+	        }
+
+	        int i = low;
+	        int j = middle + 1;
+	        int k = low;
+	        // Copy the smallest values from either the left or the right side back
+	        // to the original array
+	        while (i <= middle && j <= high) {
+	            if (helper[i] <= helper[j]) {
+	                numbers[k] = helper[i];
+	                i++;
+	            } else {
+	                numbers[k] = helper[j];
+	                j++;
+	            }
+	            k++;
+	        }
+	        // Copy the rest of the left side of the array into the target array
+	        while (i <= middle) {
+	            numbers[k] = helper[i];
+	            k++;
+	            i++;
+	        }
+	        // Since we are sorting in-place any leftover elements from the right side
+	        // are already at the right position.
+
+	    }
 	//@Override
 	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
 		// TODO Auto-generated method stub
@@ -128,8 +195,15 @@ public class JasonDavid{ //extends Contestants
 
 	//@Override
 	public double mostlySortAndGetMedian(int[] mostlySorted) {
-		// TODO Auto-generated method stub
-		return 0;
+		mSort(mostlySorted);
+		 if (mostlySorted.length%2==0) {
+			 int floor = (int)Math.floor(mostlySorted.length/2)-1;
+			 int ceiling = floor+1;
+			 return (double)(mostlySorted[floor]+mostlySorted[ceiling])/2; 
+		 }
+		 else {
+			 return mostlySorted[mostlySorted.length/2];
+		 }
 	}
 
 	//@Override
