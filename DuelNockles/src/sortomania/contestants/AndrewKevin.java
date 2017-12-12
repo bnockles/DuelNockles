e6b56130eccefd169b6db2022053cc5d23a7fcda
Andrew <az@192.168.1.144>
2017-12-12 18:11:16 -0500

    12-12

--- DuelNockles/src/sortomania/contestants/AndrewKevin.java
+++ DuelNockles/src/sortomania/contestants/AndrewKevin.java
@@ -143,7 +143,97 @@ public class AndrewKevin extends Contestant{
 	@Override
 	public int sortAndSearch(Comparable[] arr, Comparable toFind) {
 		// TODO Auto-generated method stub
+		mergeSort(arr,arr.length-1,0);
 		return 0;
+	}
+		    void merge(Comparable arr[], int l, int m, int r)
+		    {
+		        // Find sizes of two subarrays to be merged
+		        int n1 = m - l + 1;
+		        int n2 = r - m;
+		 
+		        /* Create temp arrays */
+		        Comparable L[] = new Comparable [n1];
+		        Comparable R[] = new Comparable [n2];
+		 
+		        /*Copy data to temp arrays*/
+		        for (int i=0; i<n1; ++i)
+		            L[i] = arr[l + i];
+		        for (int j=0; j<n2; ++j)
+		            R[j] = arr[m + 1+ j];
+		 
+		 
+		        /* Merge the temp arrays */
+		 
+		        // Initial indexes of first and second subarrays
+		        int i = 0, j = 0;
+		 
+		        // Initial index of merged subarry array
+		        int k = l;
+		        while (i < n1 && j < n2)
+		        {
+		            if (L[i].compareTo(R[j]) <= 0)
+		            {
+		                arr[k] = L[i];
+		                i++;
+		            }
+		            else
+		            {
+		                arr[k] = R[j];
+		                j++;
+		            }
+		            k++;
+		        }
+		 
+		        /* Copy remaining elements of L[] if any */
+		        while (i < n1)
+		        {
+		            arr[k] = L[i];
+		            i++;
+		            k++;
+		        }
+		 
+		        /* Copy remaining elements of R[] if any */
+		        while (j < n2)
+		        {
+		            arr[k] = R[j];
+		            j++;
+		            k++;
+		        }
+		    }
+		 
+		    // Main function that sorts arr[l..r] using
+		    // merge()
+		    void mergeSort(Comparable arr[], int l, int r)
+		    {
+		        if (l < r)
+		        {
+		            // Find the middle point
+		            int m = (l+r)/2;
+		 
+		            // Sort first and second halves
+		            mergeSort(arr, l, m);
+		            mergeSort(arr , m+1, r);
+		 
+		            // Merge the sorted halves
+		            merge(arr, l, m, r);
+		        }
+		    }
+	
+	//search stuff
+	public static int binarySearch(int[] nums, int n, int first, int last) {
+		if (last > first) {
+			int index=  (first + last - 1) / 2;
+			int guess = nums[index];
+			if (guess == n) {
+				return index;
+			}
+			if (guess > n) {
+				return binarySearch(nums, n, first, index);
+			}
+			return binarySearch(nums, n, index + 1, last);
+		}
+		return -1;
 	}
 
 }
