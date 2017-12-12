d07f2753e07c3bdb247a5f6319715dc59f6e3a31
sli7826 <sli7826@bths.edu>
2017-12-12 11:19:45 -0500

    Update DanSteven.java

--- DuelNockles/src/sortomania/contestants/DanSteven.java
+++ DuelNockles/src/sortomania/contestants/DanSteven.java
@@ -1,6 +1,7 @@
 package sortomania.contestants;
 
 import java.awt.Color;
+import java.util.Arrays;
 
 import sortomania.Contestant;
 
@@ -27,7 +28,15 @@ public class DanSteven extends Contestant{
 	}
 	@Override
 	public double sortMultiDim(int[][] grid) {
-		return 0;
+		int[] median=new int[grid.length];
+		int count=0;
+		for(int i=0;i<grid.length;i++) {
+			int n=grid[i].length;
+			radixsort(grid[i],n);
+			median[count]=(grid[i][n/2]+grid[i][(n-1)/2])/2;
+			count++;
+		}
+		return sortAndGetMedian(median);
 	}
 	@Override
 	public int sortAndSearch(Comparable[] arr, Comparable toFind) {
@@ -35,7 +44,63 @@ public class DanSteven extends Contestant{
 	}
 	@Override
 	public double sortAndGetMedian(int[] random) {
-		return 0;
+		int n=random.length;
+		radixsort(random,random.length);
+		return (random[n/2]+random[(n-1)/2])/2;
 	}
+	
+	public int getMax(int arr[], int n)
+	 {
+	     int mx = arr[0];
+	     for (int i = 1; i < n; i++)
+	         if (arr[i] > mx)
+	             mx = arr[i];
+	     return mx;
+	 }
+
+	 // A function to do counting sort of arr[] according to
+	 // the digit represented by exp.
+	public void countSort(int arr[], int n, int exp)
+	 {
+	     int output[] = new int[n]; // output array
+	     int i;
+	     int count[] = new int[10];
+	     Arrays.fill(count,0);
+
+	     // Store count of occurrences in count[]
+	     for (i = 0; i < n; i++)
+	         count[ (arr[i]/exp)%10 ]++;
+
+	     // Change count[i] so that count[i] now contains
+	     // actual position of this digit in output[]
+	     for (i = 1; i < 10; i++)
+	         count[i] += count[i - 1];
+
+	     // Build the output array
+	     for (i = n - 1; i >= 0; i--)
+	     {
+	         output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+	         count[ (arr[i]/exp)%10 ]--;
+	     }
+
+	     // Copy the output array to arr[], so that arr[] now
+	     // contains sorted numbers according to curent digit
+	     for (i = 0; i < n; i++)
+	         arr[i] = output[i];
+	 }
+
+	 // The main function to that sorts arr[] of size n using
+	 // Radix Sort
+	public void radixsort(int arr[], int n)
+	 {
+	     // Find the maximum number to know number of digits
+	     int m = getMax(arr, n);
+
+	     // Do counting sort for every digit. Note that instead
+	     // of passing digit number, exp is passed. exp is 10^i
+	     // where i is current digit number
+	     for (int exp = 1; m/exp > 0; exp *= 10)
+	         countSort(arr, n, exp);
+	 }
 
 }
