4a3e3c58b4f26d0b934424b8f08cb74e705bb870
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-12-12 15:31:34 -0500

    updates

--- DuelNockles/src/sortomania/contestants/DoubleJasons.java
+++ DuelNockles/src/sortomania/contestants/DoubleJasons.java
@@ -6,39 +6,97 @@ import sortomania.Contestant;
 
 public class DoubleJasons extends Contestant{
 	
-	private int[] ISSeq;
+	//private int[] ISSeq = {1, 3, 7, 21, 48, 112, 336, 861, 1968, 4592};
+	private int[] ISSeq = {1, 4, 13};
+	private static String[] names = {"Tashia","Fidela","Carley","Starla","Maisie","Elijah","Pearl","Jacquie","Zelma","Tama","Hannelore","Shaniqua","Isa","Emily","Desiree","Garnet","Lauri","Erna","Denese","Renato","Britney","Numbers","Randa","Jewel","Vincenzo","Arianna","Johnathon","Charlette","Rae","Jerald"};
 	
 	public DoubleJasons() {
-		int[] temp = {1, 3, 7, 21, 48, 112, 336, 861, 1968, 4592};
-		ISSeq = temp;
+	}
+	
+	public static final void main(String[] args) {
+		DoubleJasons test = new DoubleJasons();
+		test.shellSort(names);
 	}
 	
 	private void shellSort(String[] arr) {
-		int index = 0;
-		while() {
+		int index = ISSeq.length-1;
+		int gap = ISSeq[index];
+		String holder;
+			for(int i = 0; i< arr.length; i++) //iterate through arr
+			{
+				if(i+ gap >= arr.length)
+				{
+					i = 0;
+					index --;
+					if(index ==-1) break;
+					gap = ISSeq[index];
+				}
+				else 
+				{
+					for(int idx = 0; idx< arr[i].length()-1; idx++)
+					{
+						String char1 = arr[i].substring(idx, idx+1);
+						String char2 = arr[i+gap].substring(idx, idx+1);
+						if( char1.compareTo(char2) > 0)
+						{
+							holder = arr[i];
+							arr[i] = arr[i+gap];
+							arr[i+gap] = holder;
+							System.out.println(arr[i+gap]+"Switched with"+arr[i]);
+							break;
+						}
+						if(char1.compareTo(char2) < 0)break;
+					}
+				}
+			}
 			
+		for( String name: arr)
+		{
+			System.out.println(name);
 		}
-		
 	}
 	
-	private void shellSort(int[] random) {
-		int index = 0;
-		while() {
-			
+	private void shellSort(int[] arr) {
+		int index = ISSeq.length-1;
+		int gap = ISSeq[index];
+		int holder;
+		while( index > 0) {
+			for(int i = 0; i< arr.length; i++) //iterate through arr
+			{
+				if(i+ gap > arr.length)
+				{
+					i = 0;
+					index --;
+					gap = ISSeq[index];
+				}
+				else 
+				{
+					if(arr[i] > arr[i+gap]) {
+						holder = arr[i];
+						arr[i] = arr[i+gap];
+						arr[i+gap] = arr[i];
+					}
+					if(arr[i] == arr[i+gap]) {
+						holder = arr[i+1];
+						arr[i+1] = arr[i+gap];
+						arr[i+gap] = holder;
+					}
+				}
+			}
 		}
-		
+		System.out.print(arr);
 	}
 
 	@Override
 	public Color getColor() {
 		// TODO Auto-generated method stub
-		return null;
+		return new Color(255,0,255);
 	}
 
 	@Override
 	public String getSpriteName() {
 		// TODO Auto-generated method stub
-		return null;
+		return CHUN_LI;
 	}
 
 	@Override
