59ac95d3bfe33425b7095cfdd48d19e45ff173b5
Alice <Alice@192.168.1.135>
2017-12-13 00:02:22 -0500

    werwe

--- DuelNockles/src/sortomania/contestants/AliceDanielSort.java
+++ DuelNockles/src/sortomania/contestants/AliceDanielSort.java
@@ -1,6 +1,8 @@
 	package sortomania.contestants;
 	
 	import java.awt.Color;
+	import java.util.Arrays;
+
 import java.awt.Color;
 	
 import sortomania.Contestant;
@@ -20,58 +22,93 @@ public class AliceDanielSort extends Contestant{
 
 	@Override
 	public double sortAndGetMedian(int[] random) {
-		//put all sorting methods into one method then call it here
-		double answer = 0;
+		sort(random);
+
 		
-		int lengthOfArray = random.length;
-		//first sort using introsort then:
-		
-		if (lengthOfArray %2 == 1) {
-			
-		answer = random[(lengthOfArray + 1)/2];
-		
-			
+		if (random.length % 2 == 0) {
+			return (double)((random[random.length/2] + random[random.length/2 - 1]) / 2.0);
 		}
-		
-		else if(lengthOfArray %2 == 0 ) {
-			
-			int sum = random[lengthOfArray/2] + random[(lengthOfArray/2 )+ 1];
-			
-			answer =  sum/2;
-		}
-		return answer;
+		return (double)(random[(random.length-1) / 2]);
 	}
 
 	@Override
 	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
-		//abc order
 		
-		
-		// TODO Auto-generated method stub
-		return 0;
+		String[] strings2 = mergeSort(strings);
+		for(int i = 0; i < strings2.length;i++)
+		{
+			strings[i] = strings2[i];
+		}
+		for(int x = 0; x < strings.length;x++)
+		{
+			if(toFind.equals(strings[x]))
+			{
+				return x;
+			}
+		}
+		return -1;
 	}
 
 	@Override
 	public double mostlySortAndGetMedian(int[] mostlySorted) {
+		return sortAndGetMedian(mostlySorted);
 		
-		// use introsort as well
-		
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
-	@Override
+	@  Override
 	public double sortMultiDim(int[][] grid) {
-		// TODO Auto-generated method stub
-		return 0;
+		int[] medianArr = new int[grid[0].length];
+		for (int x = 0; x < grid.length; x++) {
+			medianArr[x]= (int) sortAndGetMedian(grid[x]);
+		}
+		int[] medianArrPos = new int[medianArr.length];
+		for (int y = 0; y < medianArrPos.length; y++) {
+			medianArrPos[y] = y;
+		}
+		bubbleSort(medianArr, medianArrPos);
+		
+//		System.out.println("Median Array Sorted: ");
+//		for (int z = 0; z < medianArrPos.length; z++) {
+//			System.out.print(medianArr[z]+ " ");
+//		}
+//		System.out.println("Median Array Positions:");
+//		for (int z = 0; z < medianArrPos.length; z++) {
+//			System.out.print(medianArrPos[z]+ " ");
+//		}
+		for(int i = 0; i < medianArrPos.length; i++)
+		{
+			int[] temp = new int [grid[0].length];
+			temp = grid[i];
+			grid[i] = grid[medianArrPos[i]];
+			grid[medianArrPos[i]] = temp;
+		}
+		
+		
+		for(int x = 0;x < grid[0].length; x++){
+            for(int y = 0; y < grid.length/2; y++) {
+                int temp = grid[y][x];
+                grid[y][x] = grid[grid.length - y - 1][x];
+                grid[grid.length - y - 1][x] = temp;
+            }
+		}
+		return sortAndGetMedian(medianArr);
 	}
 
 	@Override
 	public int sortAndSearch(Comparable[] arr, Comparable toFind) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		bubbleSort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].equals(toFind)) {
+				return i;
+			}
+		}
+		return -1;
+		
+		
 	}
-
+	
+	
 	public static void sort(int[] a)
     {
       introsort_loop(a, 0, a.length, 2*floor_lg(a.length));
@@ -214,7 +251,90 @@ private static int floor_lg(int a)
   {
     return (int)(Math.floor(Math.log(a)/Math.log(2)));
   }
+public String[] mergeSort(String[] list) {
+	if (list.length == 1) {
+		return list;
+	}
+	else {
+		String[] newList = Arrays.copyOfRange(list,0,list.length / 2);
+		String[] newList2 = Arrays.copyOfRange(list,list.length / 2,list.length);
+		return merge(mergeSort(newList),mergeSort(newList2));
+		
+	}
+}
+public String[] merge(String[] list1, String[] list2) {
+	String[] list3 = new String[list1.length + list2.length];
+	int x = 0;
+	int y = 0;
+	int index = 0;
+	while(x < list1.length || y < list2.length)
+	{
+		if(x == list1.length) //full of x
+		{
+			list3[index] = list2[y];
+			y++;
+			index++;
+		}
+		else if(y == list2.length) //full of y
+		{
+			list3[index] = list1[x];
+			x++;
+			index++;
+		}
+		else if(list1[x].compareTo(list2[y]) < 0) // if list1's item is smaller than list2's item
+		{
+			list3[index]= list1[x];
+			x++;
+			index++;
+		}
+		else
+		{
+			list3[index]= list2[y];
+			y++;
+			index++;
+		}
+	}
+	return list3;
+}
+public static void bubbleSort(int[] medianArr, int[] medianArrPos) {
+	int counter = medianArr.length-1;
+	while (counter > 0) {
+		for (int x = 0; x < medianArr.length-1; x++) {
+			for (int y = x+1; y < medianArr.length; y++) {
+				if (medianArr[x] > (medianArr[y])) {
+					int temp = medianArr[x];
+					medianArr[x] = medianArr[y];
+					medianArr[y] = temp;
+					int temp2 = medianArrPos[x];
+					medianArrPos[x] = medianArrPos[y];
+					medianArrPos[y] = temp2;
+				}
+				else {
+					counter--;
+				}
+			}
+		}
+	}	
+}
 
-	
+public static void bubbleSort(Comparable[] arr)
+{
+	boolean change = false;
+	while (!change) {
+		int numChanges = 0;
+		for (int x = 0; x < arr.length-1; x++) {
+			Comparable temp;
+			if (arr[x].compareTo(arr[x+1]) > 0) {
+				temp = arr[x+1];
+				arr[x+1] = arr[x];
+				arr[x] = temp;
+				numChanges++;
+			}		
+		}
+		if (numChanges == 0) {
+			change = true;
+		}
+	}
+} 
 	
 }
