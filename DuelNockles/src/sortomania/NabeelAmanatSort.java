0deda6aa5b0ac2774679a9aacb073d7ba18a362b
Nabeel Amin <Nabeel Amin@DESKTOP-O9NHAK6>
2017-12-13 07:46:19 -0500

    finsihed

--- DuelNockles/src/sortomania/NabeelAmanatSort.java
+++ DuelNockles/src/sortomania/NabeelAmanatSort.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 
 public class NabeelAmanatSort extends Contestant {
 
-	private static Comparable[] unsorted = {2,555,3,67,9999,45,32,34,576,675,756,565,808,654,6878,4543,1000,0111,1113,4535};
+	/*private static Comparable[] unsorted = {2,555,3,67,9999,45,32,34,576,675,756,565,808,654,6878,4543,1000,0111,1113,4535};
 	private static Comparable[] stringList= {"bcde","Abcd", "cdef", "Mnlo", "katie","jklmn"};
-	private static int[][] twodim= {{34,23,34,32,34}, {75,5454, 45,34,45,45,4,5,7,6,808,654,6878,4543,1000,0111,1113,0,345},{45,999,998,776,55,444,33,6565,1453,3575}};
+	private static int[][] twodim= {{34,23,34,32,34}, {75,5454, 45,34,45,45,4,5,7,6,808,654,6878,4543,1000,0111,1113,0,345},{45,999,998,776,55,444,33,6565,1453,3575}};*/
 	private int index =0; 
 	
 	public static void main(String[] args) {
-		NabeelAmanatSort test = new NabeelAmanatSort(); 
+		//NabeelAmanatSort test = new NabeelAmanatSort(); 
 		//First test 
 		/*System.out.println(Arrays.toString(unsorted));
 		double x = test.sortAndGetMedian(unsorted);
@@ -19,10 +19,10 @@ public class NabeelAmanatSort extends Contestant {
 		System.out.println(Arrays.toString(unsorted));*/
 		
 		//Second test
-		System.out.println(Arrays.toString(stringList));
+		/*System.out.println(Arrays.toString(stringList));
 		int x = test.sortAndSearch(unsorted, 34);
 		System.out.println(x);
-		System.out.println(Arrays.toString(stringList));
+		System.out.println(Arrays.toString(stringList));*/
 
 		
 		//third test
@@ -42,7 +42,6 @@ public class NabeelAmanatSort extends Contestant {
 			System.out.println(Arrays.toString(twodim[i])+"\n");
 		}*/
 		
-		//Fifth test
 	}
 	@Override
 	public Color getColor() {
@@ -115,31 +114,6 @@ public class NabeelAmanatSort extends Contestant {
 	        arr[i] = arr[j];
 	        arr[j] = temp;
 	    }
-	/*public void testQuickSort() {
-	        for (Integer i : unsorted) {
-	            System.out.println(i + " ");
-	        }
-	        long startTime = System.currentTimeMillis();
-
-	        NabeelAmanatSort sorter = new NabeelAmanatSort();
-	        
-	        long stopTime = System.currentTimeMillis();
-	        long elapsedTime = stopTime - startTime;
-	        System.out.println("Quicksort " + elapsedTime);
-
-	        if (!validate(unsorted)) {
-	            System.out.println("Should not happen");
-	        }
-	    }
-	private boolean validate(int[] numbers) {
-	        for (int i = 0; i < numbers.length - 1; i++) {
-	            if (numbers[i] > numbers[i + 1]) {
-	                return false;
-	            }
-	        }
-	        return true;
-	    }
-	*/
 	
 	
 	//Second thing 
@@ -274,50 +248,43 @@ public class NabeelAmanatSort extends Contestant {
 	@Override
 	
 	public int sortAndSearch(Comparable[] arr, Comparable toFind) {
-		if(!(arr[0] instanceof Integer)) {
-			boolean swapped;
-			for(int a =0 ; a<arr.length; a++) {
-				swapped =false;
-				for(int i =0; i<arr.length-1; i++) {
-					if(((String) arr[i]).toLowerCase().compareTo(((String) arr[i+1]).toLowerCase()) > 0){
-						exchange(arr, i, i+1);
-						swapped = true;
-					}
-					
-				};
-				if(swapped = false) {
-					break; 
-				}
-			}
-		}
-		
-		else {
-			boolean swapped;
-			for(int a =0 ; a<arr.length; a++) {
-				swapped =false;
-				for(int i =0; i<arr.length-1; i++) {
-					if(arr[i].compareTo(arr[i+1]) > 0){
-						exchange(arr, i, i+1);
-						swapped = true;
-					}
-					
-				}
-				if(swapped = false) {
-					break; 
-				}
-		}
-		}
-		for(int i =0; i<arr.length; i++) {
-			if(arr[i].equals(toFind)) {
+		quickSort(arr,0,arr.length-1);
+		for(int i = 0; i < arr.length;i++){
+			if(arr[i].equals(toFind)){
 				return i;
 			}
 		}
 		return -1;
-	}
-	public void exchange(Comparable[] arr, int i, int j) {
-        Comparable temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+	} 
+	 public void quickSort(Comparable[] a, int lo, int hi) {
+	       if(lo >= hi) return;
+	       int pi = partition(a, lo, hi);
+	       quickSort(a, lo, pi-1);
+	       quickSort(a, pi+1, hi);
+	   }
+	 private int partition(Comparable[] a, int lo, int hi) {
+	       int i = lo + 1;
+	       int j = hi;
+	       while(i <= j) {
+	           if(a[i].compareTo(a[lo]) <= 0) { 
+	               i++; 
+	           }
+	           else if(a[j].compareTo(a[lo]) > 0) { 
+	               j--;
+	           }
+	           else if(j < i) {
+	               break;
+	           }
+	           else
+	              swap(a, i, j);
+	       }
+	       swap(a, lo, j);
+	       return j;
+	   }
+	 private void swap(Comparable[] a, int i, int j) {
+		   Comparable temp = a[i];
+	       a[i] = a[j];
+	       a[j] = temp;
+	   }
 	}
 
