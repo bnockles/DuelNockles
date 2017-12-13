8bd062b73cb973f5e66024498517f3dc69efa3e3
yangj <yangj@LAPTOP-HMQEEB7B.fios-router.home>
2017-12-12 20:41:13 -0500

    updates

--- DuelNockles/src/sortomania/contestants/DoubleJasons.java
+++ DuelNockles/src/sortomania/contestants/DoubleJasons.java
@@ -8,13 +8,13 @@ public class DoubleJasons extends Contestant{
 	
 	private int[] ISSeq = {1,3,7,21,48,112,336,861,1968,4592};
 	private static String[] names = {"Tashia","Fidela","Carley","Starla","Maisie","Elijah","Pearl","Jacquie","Zelma","Tama","Hannelore","Shaniqua","Isa","Emily","Desiree","Garnet","Lauri","Erna","Denese","Renato","Britney","Numbers","Randa","Jewel","Vincenzo","Arianna","Johnathon","Charlette","Rae","Jerald"};
-	
+	private static int[] nums = {1,2312,12312,35435,345357,7767,657,6765,76,576,5765765,765,76,57,657,65,765,765,765,7,657,657569,789,79,907,78,9,879,879,78};
 	public DoubleJasons() {
 	}
 	
 	public static final void main(String[] args) {
 		DoubleJasons test = new DoubleJasons();
-		test.shellSort(names);
+		test.shellSort(nums);
 	}
 	
 	private void shellSort(String[] arr) {
@@ -64,7 +64,7 @@ public class DoubleJasons extends Contestant{
 		while( index > 0) {
 			for(int i = 0; i< arr.length; i++) //iterate through arr
 			{
-				if(i+ gap > arr.length)
+				if(i+ gap >= arr.length)
 				{
 					i = 0;
 					index --;
@@ -88,6 +88,37 @@ public class DoubleJasons extends Contestant{
 		System.out.print(arr);
 	}
 
+	private void shellSort(double[] arr) {
+		int index = ISSeq.length-1;
+		int gap = ISSeq[index];
+		double holder;
+		while( index > 0) {
+			for(int i = 0; i< arr.length; i++) //iterate through arr
+			{
+				if(i+ gap >= arr.length)
+				{
+					i = 0;
+					index --;
+					gap = ISSeq[index];
+				}
+				else 
+				{
+					if(arr[i] > arr[i+gap]) {
+						holder = arr[i];
+						arr[i] = arr[i+gap];
+						arr[i+gap] = arr[i];
+					}
+					if(arr[i] == arr[i+gap]) {
+						holder = arr[i+1];
+						arr[i+1] = arr[i+gap];
+						arr[i+gap] = holder;
+					}
+				}
+			}
+		}
+		System.out.print(arr);
+	}
+	
 	private void checkPreviousIndices(int index, int gap, String[] arr) {
 		while(index - gap >= 0) {
 			for(int secondCheck = 0; secondCheck< arr[index].length()-1; secondCheck++)
@@ -122,25 +153,38 @@ public class DoubleJasons extends Contestant{
 	@Override
 	public double sortAndGetMedian(int[] random) {	
 		shellSort(random);
-		return random[random.length/2];
+		double half = random.length/2;
+		return (random[(int) Math.floor(half)] + random[(int) Math.ceil(half)])/2;
 	}
 
+	public double sortAndGetMedian(double[] medians) {
+		shellSort(medians);
+		double half = medians.length/2;
+		return (medians[(int) Math.floor(half)] + medians[(int) Math.ceil(half)])/2;
+	}
+	
 	@Override
 	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
-		// TODO Auto-generated method stub
-		return 0;
+		shellSort(strings);
+		for(int i = 0; i < strings.length; i++) {
+			if(strings[i].equals(toFind)) return i;
+		}
+		return -1;
 	}
 
 	@Override
 	public double mostlySortAndGetMedian(int[] mostlySorted) {
-		// TODO Auto-generated method stub
-		return 0;
+		shellSort(mostlySorted);
+		return mostlySorted[mostlySorted.length/2];
 	}
 
 	@Override
 	public double sortMultiDim(int[][] grid) {
-		// TODO Auto-generated method stub
-		return 0;
+		double[] medians = new double[grid.length];
+		for (int i = 0; i < grid.length; i++) {
+			medians[i] = sortAndGetMedian(grid[i]);
+		}
+		return sortAndGetMedian(medians);
 	}
 
 	@Override
