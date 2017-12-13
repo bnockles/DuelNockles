1ed7570497be24db647f51b8d84b1c67f23d0ad1
TylerOvenden <tovenden4386@bths.edu>
2017-12-12 23:56:16 -0500

    Update CarsonSort.java
    added doubles

--- DuelNockles/src/sortomania/CarsonSort.java
+++ DuelNockles/src/sortomania/CarsonSort.java
@@ -7,18 +7,18 @@ public class CarsonSort extends Contestant{
 
 	public static void main(String a[]){
 		CarsonSort test = new CarsonSort();
-		//		int[][] input = {{10,2,5,1,6}, {3,7,6,4,7,10}, {2,5,3,1,6}, {4,8,1,2,6}, {4,23,5,23,12}};
+	int[][] input = {{10,2,5,1,6}, {3,7,6,4,7,0}, {6,40,30,2034,4,3,20,14,15}, {11,1,30,9,340,34,20,2}};
 		String[] test1 = {"abc", "zaq", "baq", "Aba", "C", "l", "ii"};
-
-		System.out.println(Arrays.toString(test1));
-		System.out.println(test.sortAndSearch(test1,"huiho"));
-		System.out.println(Arrays.toString(test1));
+		
+			System.out.println(Arrays.toString(input[0]) + Arrays.toString(input[1]) + Arrays.toString(input[2]) + Arrays.toString(input[3]));
+			System.out.println(test.sortMultiDim(input));
+			System.out.println(Arrays.toString(input[0]) + Arrays.toString(input[1]) + Arrays.toString(input[2]) + Arrays.toString(input[3]));
 	}
 
 	public Color getColor() {
 		return new Color(123,123,123);
 	}
-
+	
 	public String getSpriteName() {
 		return KEN;
 	}
@@ -26,17 +26,20 @@ public class CarsonSort extends Contestant{
 	public double sortAndGetMedian(int[] random) {
 		quickSort(random, 0, random.length-1);
 		if(random.length % 2 == 0) {
-			return (random[(int)random.length /2]+random[((int)random.length /2)+1])/2;
+			return (double)(random[random.length/2] + random[random.length/2 - 1])/2;
 		}
 		else {
-			return random[(int)random.length /2];
+			return (double)random[(random.length-1)/2];
 		}
 	}
 
 	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
-		for (int i=0; i<strings.length;i++) {
-			for (int j=i+1 ; j<strings.length; j++) {
-				if (strings[j].compareTo(strings[i])<0) {
+		for (int i=0; i<strings.length;i++)
+		{
+			for (int j=i+1 ; j<strings.length; j++)
+			{
+				if (strings[j].compareTo(strings[i])<0)
+				{
 					String k = strings[i];
 					strings[i] = strings[j];
 					strings[j] = k;
@@ -50,7 +53,7 @@ public class CarsonSort extends Contestant{
 		}
 		return -1;
 	}
-
+	
 	void insertSort(int arr[])
 	{
 		int n = arr.length;
@@ -69,23 +72,74 @@ public class CarsonSort extends Contestant{
 		insertSort(mostlySorted);
 		printArray(mostlySorted);
 		if(mostlySorted.length % 2 == 0) {
-			return (mostlySorted[(int)mostlySorted.length /2]+mostlySorted[((int)mostlySorted.length /2)+1])/2;
+			return (double)(mostlySorted[mostlySorted.length/2] + mostlySorted[mostlySorted.length/2 - 1])/2;
 		}
-		return mostlySorted[(int)mostlySorted.length /2];
+		return (double)mostlySorted[(mostlySorted.length-1)/2];
 	}
 
 	public double sortMultiDim(int[][] grid) {
-		int[] arr = new int[grid.length];
-		for(int i = 0; i < grid.length; i ++) {
-			arr[i] = (int) sortAndGetMedian(grid[i]);
+		double[] arr = new double[grid.length];
+		for(int i = 0; i < grid.length; i++) {
+			arr[i] = sortAndGetMedian(grid[i]);
 		}
 		return sortAndGetMedian(arr);
 	}
 
+	private double sortAndGetMedian(double[] random) {
+		quickSort(random, 0, random.length-1);
+		if(random.length % 2 == 0) {
+			return (double)(random[random.length/2] + random[random.length/2 - 1])/2;
+		}
+		else {
+			return (double)random[(random.length-1)/2];
+		}
+	}
+
+	private void quickSort(double[] random, int lowerIndex, int higherIndex) {
+		if (random == null || random.length == 0) {
+			return;
+		}
+		int i = lowerIndex;
+		int j = higherIndex;
+		double pivot = random[lowerIndex+(higherIndex-lowerIndex)/2];
+		while (i <= j) {
+			while (random[i] < pivot) {
+				i++;
+			}
+			while (random[j] > pivot) {
+				j--;
+			}
+			if (i <= j) {
+				swap(random, i, j);
+				i++;
+				j--;
+			}
+		}
+		if (lowerIndex < j) {
+			quickSort(random, lowerIndex, j);
+		}
+		if (i < higherIndex) {
+			quickSort(random, i, higherIndex);
+		}
+	}
+	
+		
+	
+
+	private void swap(double[] random, int i, int j) {
+		double temp = random[i];
+		random[i] = random[j];
+		random[j] = temp;
+		
+	}
+
 	public int sortAndSearch(Comparable[] arr, Comparable toFind) {
-		for (int i=0; i<arr.length;i++) {
-			for (int j=i+1 ; j<arr.length; j++) {
-				if (arr[j].compareTo(arr[i])<0) {
+		for (int i=0; i<arr.length;i++)
+		{
+			for (int j=i+1 ; j<arr.length; j++)
+			{
+				if (arr[j].compareTo(arr[i])<0)
+				{
 					Comparable k = arr[i];
 					arr[i] = arr[j];
 					arr[j] = k;
@@ -126,6 +180,8 @@ public class CarsonSort extends Contestant{
 			quickSort(intArr, i, higherIndex);
 		}
 	}
+	
+	
 
 	static void printArray(int arr[]) {
 		int n = arr.length;
@@ -141,4 +197,3 @@ public class CarsonSort extends Contestant{
 		arr[j] = temp;
 	}
 }
-
