4c5e5d9672285cebb9677248678c48a2c7e2422b
user <user@192.168.1.4>
2017-12-12 22:15:42 -0500

    sortMultiDim problem

--- DuelNockles/src/sortomania/contestants/VincentNRaymond.java
+++ DuelNockles/src/sortomania/contestants/VincentNRaymond.java
@@ -12,11 +12,8 @@ public class VincentNRaymond extends Contestant {
 		VincentNRaymond test = new VincentNRaymond();
 		int[] arr = { 4, 7, 10, 2, 18, 12, 34, 42, 23, 40, 56, 31, 8, 9, 124, 562, 26 };
 		System.out.println("The median is: " + test.sortAndGetMedian(arr));
-		
 
 		System.out.println("And the sorted array is: \n" + Arrays.toString(sort(arr)));
-		
-		
 
 	}
 
@@ -76,19 +73,23 @@ public class VincentNRaymond extends Contestant {
 
 	@Override
 	public double sortAndGetMedian(int[] random) {
+		int[][] darr = new int[2][2];
+		darr[0][0] = 5;
+		darr[0][1] = 6;
+		darr[1][0] = 6;
+		darr[1][1] = 3;
+		System.out.println("DOUBLE MEDIAN: " + sortMultiDim(darr));
+		
 		int[] arr = { 4, 7, 10, 2, 18, 12, 34, 42, 23, 40, 56, 31, 8, 9, 124, 562, 26 };
-		System.out.println("MOSTLY SORTED:"+ mostlySortAndGetMedian(arr));
-		
-		
-		
-		String[] strings =  {"zz","ab", "aa"};
+		System.out.println("MOSTLY SORTED:" + mostlySortAndGetMedian(arr));
+
+		String[] strings = { "zz", "ab", "aa" };
 		System.out.println(sortAndGetResultingIndexOf(strings, "ab"));
-		
-		
+
 		// TODO Auto-generated method stub
 		int[] sortedArray = sort(random);
 		return getMedian(sortedArray);
-		
+
 	}
 
 	public double getMedian(int[] sortedArray) {
@@ -98,50 +99,51 @@ public class VincentNRaymond extends Contestant {
 			return ((sortedArray[sortedArray.length / 2]) + (sortedArray[(sortedArray.length / 2) - 1])) / 2;
 		}
 	}
-	
-	  public static int partition(String[] stringArray, int idx1, int idx2) {
-	        String pivotValue = stringArray[idx1];
-	        while (idx1 < idx2) {
-	           String value1;
-	           String value2;
-	           while ((value1 = stringArray[idx1]).compareTo( pivotValue ) < 0) {
-	               idx1 = idx1 + 1;
-	           }
-	           while ((value2 = stringArray[idx2]).compareTo( pivotValue ) > 0) {
-	               idx2 = idx2 - 1;
-	           }
-	           stringArray[idx1] = value2;
-	           stringArray[idx2] = value1;
-	        }
-	        return idx1;
-	    }
-	    public static void QuicksortString(String[] stringArray, int idx1, int idx2) {
-	        if (idx1 >= idx2) {
-	            // we are done
-	            return;
-	        }
-	        int pivotIndex = partition(stringArray, idx1, idx2);
-	        QuicksortString(stringArray, idx1, pivotIndex);
-	        QuicksortString(stringArray, pivotIndex+1, idx2);
-	     }
-	    public static void QuicksortString(String[] stringArray) {
-	        QuicksortString(stringArray, 0, stringArray.length - 1);
-	    }
-	
-	
+
+	public static int partition(String[] stringArray, int idx1, int idx2) {
+		String pivotValue = stringArray[idx1];
+		while (idx1 < idx2) {
+			String value1;
+			String value2;
+			while ((value1 = stringArray[idx1]).compareTo(pivotValue) < 0) {
+				idx1 = idx1 + 1;
+			}
+			while ((value2 = stringArray[idx2]).compareTo(pivotValue) > 0) {
+				idx2 = idx2 - 1;
+			}
+			stringArray[idx1] = value2;
+			stringArray[idx2] = value1;
+		}
+		return idx1;
+	}
+
+	public static void QuicksortString(String[] stringArray, int idx1, int idx2) {
+		if (idx1 >= idx2) {
+			// we are done
+			return;
+		}
+		int pivotIndex = partition(stringArray, idx1, idx2);
+		QuicksortString(stringArray, idx1, pivotIndex);
+		QuicksortString(stringArray, pivotIndex + 1, idx2);
+	}
+
+	public static void QuicksortString(String[] stringArray) {
+		QuicksortString(stringArray, 0, stringArray.length - 1);
+	}
+
 	@Override
 	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
 		QuicksortString(strings);
-		if(toFind.substring(0, 1).compareTo("A") > -1000) {
-			//start from the end
-			for(int i = strings.length - 1 ; i >= 0; i--) {
-				if(strings[i].compareTo(toFind) == 0) {
+		if (toFind.substring(0, 1).compareTo("A") > -1000) {
+			// start from the end
+			for (int i = strings.length - 1; i >= 0; i--) {
+				if (strings[i].compareTo(toFind) == 0) {
 					return i;
 				}
 			}
 		} else {
-			for(int i = 0 ; i < strings.length - 1; i++) {
-				if(strings[i].compareTo(toFind) == 0) {
+			for (int i = 0; i < strings.length - 1; i++) {
+				if (strings[i].compareTo(toFind) == 0) {
 					return i;
 				}
 			}
@@ -150,33 +152,29 @@ public class VincentNRaymond extends Contestant {
 	}
 
 	public void insertionSort(int[] arr) {
-		 int n = arr.length;
-	        for (int i=1; i<n; ++i)
-	        {
-	            int key = arr[i];
-	            int j = i-1;
-	 
-	            /* Move elements of arr[0..i-1], that are
-	               greater than key, to one position ahead
-	               of their current position */
-	            while (j>=0 && arr[j] > key)
-	            {
-	                arr[j+1] = arr[j];
-	                j = j-1;
-	            }
-	            arr[j+1] = key;
-	        }
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int key = arr[i];
+			int j = i - 1;
+
+			/*
+			 * Move elements of arr[0..i-1], that are greater than key, to one position
+			 * ahead of their current position
+			 */
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
 	}
-	
+
 	@Override
 	public double mostlySortAndGetMedian(int[] mostlySorted) {
 		insertionSort(mostlySorted);
 		return getMedian(mostlySorted);
 	}
 
-
-	
-	
 	@Override
 	public double sortMultiDim(int[][] grid) {
 		// TODO Auto-generated method stub
@@ -192,8 +190,28 @@ public class VincentNRaymond extends Contestant {
 			medianHolder[count] = sortAndGetMedian(medianHolder2);
 			count++;
 		}
-		
-		return 0;
+		boolean sorted=true;
+	  	double temp = 0;
+
+	  while (sorted){
+	     sorted = false;
+	     for (int i=0; i < medianHolder.length-1; i++) 
+	        if (medianHolder[i] > medianHolder[i+1]) {                      
+	           temp       = medianHolder[i];
+	           medianHolder[i]       = medianHolder[i+1];
+	           medianHolder[i+1]     = temp;
+	           sorted = true;
+	        }     
+	  }
+		return getMedian(medianHolder);
+	}
+
+	private double getMedian(double[] sortedArray) {
+		if (sortedArray.length % 2 == 1) {
+			return sortedArray[(sortedArray.length - 1) / 2];
+		} else {
+			return ((sortedArray[sortedArray.length / 2]) + (sortedArray[(sortedArray.length / 2) - 1])) / 2;
+		}
 	}
 
 	@Override
